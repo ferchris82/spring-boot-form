87024a964a7e9a7557eee6546208df166e814a7c
ChrisferDev <ferchris82@hotmail.com>
2024-03-11 13:10:11 -0500

    Versión 2.9

--- src/main/java/com/bolsadeideas/springoboot/app/springbootform/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/bolsadeideas/springoboot/app/springbootform/interceptors/TiempoTranscurridoInterceptor.java
@@ -1,5 +1,9 @@
 package com.bolsadeideas.springoboot.app.springbootform.interceptors;
 
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,17 +14,38 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-            ModelAndView modelAndView) throws Exception {
-
-    }
+    private static final Logger logger = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
 
+        logger.info("TiempoTranscurridoInterceptor: preHandle() entrando ...");
+        long tiempoInicio = System.currentTimeMillis();
+        request.setAttribute("tiempoInicio", tiempoInicio);
+
+        Random random = new Random();
+        Integer demora = random.nextInt(500);
+        Thread.sleep(demora);
+        
         return true;
     }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+
+                long tiempoFin = System.currentTimeMillis();
+                long tiempoInicio = (Long)request.getAttribute("tiempoInicio");
+                long tiempoTranscurrido = tiempoFin - tiempoInicio;
+
+                if(modelAndView != null){
+                    modelAndView.addObject("tiempoTranscurrido", tiempoTranscurrido);
+                }
+                logger.info("Tiempo Transcurrido: " + tiempoTranscurrido + " milisegundos");  
+                logger.info("TiempoTranscurridoInterceptor: preHandle() entrando ...");
+    }
+
+    
 
 }
