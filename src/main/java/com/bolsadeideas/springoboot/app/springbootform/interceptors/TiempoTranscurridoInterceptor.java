5e5e98de43bdf069011669112dbf7359ba01fbc8
ChrisferDev <ferchris82@hotmail.com>
2024-03-11 13:39:01 -0500

    Versión 3.0

--- src/main/java/com/bolsadeideas/springoboot/app/springbootform/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/bolsadeideas/springoboot/app/springbootform/interceptors/TiempoTranscurridoInterceptor.java
@@ -5,13 +5,14 @@ import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
+@Component("tiempoTranscurridoInterceptor")
 public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class);
@@ -20,14 +21,20 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
 
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod metodo = (HandlerMethod)handler;
+            logger.info("es un método del controlador: " + metodo.getMethod().getName());
+        }
+
         logger.info("TiempoTranscurridoInterceptor: preHandle() entrando ...");
+        logger.info("Interceptando: " + handler);
         long tiempoInicio = System.currentTimeMillis();
         request.setAttribute("tiempoInicio", tiempoInicio);
 
         Random random = new Random();
         Integer demora = random.nextInt(500);
         Thread.sleep(demora);
-        
+
         return true;
     }
 
@@ -35,17 +42,16 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             ModelAndView modelAndView) throws Exception {
 
-                long tiempoFin = System.currentTimeMillis();
-                long tiempoInicio = (Long)request.getAttribute("tiempoInicio");
-                long tiempoTranscurrido = tiempoFin - tiempoInicio;
+        long tiempoFin = System.currentTimeMillis();
+        long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
+        long tiempoTranscurrido = tiempoFin - tiempoInicio;
 
-                if(modelAndView != null){
-                    modelAndView.addObject("tiempoTranscurrido", tiempoTranscurrido);
-                }
-                logger.info("Tiempo Transcurrido: " + tiempoTranscurrido + " milisegundos");  
-                logger.info("TiempoTranscurridoInterceptor: preHandle() entrando ...");
+        if (handler instanceof HandlerMethod && modelAndView != null) {
+            modelAndView.addObject("tiempoTranscurrido", tiempoTranscurrido);
+        }
+
+        logger.info("Tiempo Transcurrido: " + tiempoTranscurrido + " milisegundos");
+        logger.info("TiempoTranscurridoInterceptor: postHandle() saliendo ...");
     }
-
-    
 
 }
